Gradient accumulation - bugfix. Deeper nets. BatchNorm.

aste/models/models.py
@@ -7,7 +7,6 @@ from ASTE.aste.models.model_elements.embeddings import Bert, BaseEmbedding
 from ASTE.aste.models.model_elements.span_aggregators import BaseAggregator, EndPointAggregator
 from ASTE.aste.models import ModelOutput, ModelLoss, ModelMetric
 from ASTE.dataset.reader import Batch
-from ASTE.utils import config
 
 
 class BaseModel(torch.nn.Module):
@@ -65,12 +64,11 @@ class BertBaseModel(BaseModel):
 
         super(BertBaseModel, self).__init__(model_name)
         self.each_part_epochs: Dict = {
-            'chunker selector': 5,
-            'triplet': 10
+            'chunker and selector': 10,
+            'triplet': 5
         }
 
-        self.embeddings_layer_chunker: BaseEmbedding = Bert(model_name='Chunker Bert')
-        self.embeddings_layer_matrix: BaseEmbedding = Bert(model_name='Matrix Bert')
+        self.embeddings_layer_chunker: BaseEmbedding = Bert()
         self.chunker: BaseModel = ChunkerModel(input_dim=self.embeddings_layer_chunker.embedding_dim)
         self.aggregator: BaseAggregator = EndPointAggregator(input_dim=self.embeddings_layer_chunker.embedding_dim)
         self.span_selector: BaseModel = Selector(input_dim=self.aggregator.output_dim)
@@ -78,16 +76,14 @@ class BertBaseModel(BaseModel):
 
     def forward(self, batch: Batch) -> ModelOutput:
         embeddings_chunker: torch.Tensor = self.embeddings_layer_chunker(batch.sentence, batch.mask)
-        embeddings_matrix: torch.Tensor = self.embeddings_layer_matrix(batch.sentence, batch.mask)
 
         chunker_output: torch.Tensor = self.chunker(embeddings_chunker)
         predicted_spans: List[torch.Tensor] = self.chunker.get_spans_from_batch_prediction(batch, chunker_output)
 
         agg_emb_chunker: torch.Tensor = self.aggregator.aggregate(embeddings_chunker, predicted_spans)
-        agg_emb_matrix: torch.Tensor = self.aggregator.aggregate(embeddings_matrix, predicted_spans)
 
         span_selector_output: torch.Tensor = self.span_selector(agg_emb_chunker)
-        triplet_input: torch.Tensor = span_selector_output[..., 1:] * agg_emb_matrix
+        triplet_input: torch.Tensor = span_selector_output[..., 1:] * agg_emb_chunker
 
         triplet_results: torch.Tensor = self.triplets_extractor(triplet_input)
 
@@ -119,24 +115,21 @@ class BertBaseModel(BaseModel):
 
     def update_trainable_parameters(self) -> None:
         self.performed_epochs += 1
-        if self.performed_epochs < self.each_part_epochs['chunker selector']:
+        if self.performed_epochs < self.each_part_epochs['chunker and selector']:
             self.fully_trainable = False
             self.triplets_extractor.freeze()
             self.span_selector.unfreeze()
             self.chunker.unfreeze()
             self.embeddings_layer_chunker.unfreeze()
-            self.embeddings_layer_matrix.freeze()
-        elif self.performed_epochs < (self.each_part_epochs['triplet'] + self.each_part_epochs['chunker selector']):
+        elif self.performed_epochs < (self.each_part_epochs['triplet'] + self.each_part_epochs['chunker and selector']):
             self.fully_trainable = False
             self.chunker.freeze()
             self.span_selector.freeze()
             self.embeddings_layer_chunker.freeze()
-            self.embeddings_layer_matrix.unfreeze()
             self.triplets_extractor.unfreeze()
-        elif self.performed_epochs >= (self.each_part_epochs['triplet'] + self.each_part_epochs['chunker selector']):
+        elif self.performed_epochs >= (self.each_part_epochs['triplet'] + self.each_part_epochs['chunker and selector']):
             self.fully_trainable = True
             self.triplets_extractor.unfreeze()
             self.span_selector.unfreeze()
             self.chunker.unfreeze()
             self.embeddings_layer_chunker.unfreeze()
-            self.embeddings_layer_matrix.unfreeze()

aste/models/specialty_models/chunker_model.py
@@ -20,14 +20,15 @@ class ChunkerModel(BaseModel):
                                       ignore_index=ChunkCode.NOT_RELEVANT).to(config['general']['device'])
 
         self.dropout = torch.nn.Dropout(0.1)
-        self.linear_layer_1 = torch.nn.Linear(input_dim, 100)
-        self.linear_layer_2 = torch.nn.Linear(100, 10)
+        self.linear_layer_1 = torch.nn.Linear(input_dim, 400)
+        self.linear_layer_2 = torch.nn.Linear(400, 100)
+        self.linear_layer_3 = torch.nn.Linear(100, 10)
         self.final_layer = torch.nn.Linear(10, 2)
         self.softmax = torch.nn.Softmax(dim=-1)
 
     def forward(self, data: torch.Tensor) -> torch.Tensor:
         layer: torch.nn.Linear
-        for layer in [self.linear_layer_1, self.linear_layer_2]:
+        for layer in [self.linear_layer_1, self.linear_layer_2, self.linear_layer_3]:
             data = layer(data)
             data = torch.relu(data)
             data = self.dropout(data)

aste/models/specialty_models/selection_model.py
@@ -22,13 +22,22 @@ class Selector(BaseModel):
         self.metrics: Metric = Metric(name='Span Selector Metrics', metrics=metrics,
                                       ignore_index=self._ignore_index).to(config['general']['device'])
 
-        self.linear_layer1 = torch.nn.Linear(input_dim, 100)
+        self.dropout = torch.nn.Dropout(0.1)
+        self.batch_norm = torch.nn.BatchNorm2d(input_dim)
+        self.linear_layer_1 = torch.nn.Linear(input_dim, 500)
+        self.linear_layer_2 = torch.nn.Linear(500, 250)
+        self.linear_layer_3 = torch.nn.Linear(250, 100)
         self.final_layer = torch.nn.Linear(100, 2)
         self.softmax = torch.nn.Softmax(dim=-1)
 
     def forward(self, data: torch.Tensor) -> torch.Tensor:
-        data = self.linear_layer1(data)
-        data = torch.relu(data)
+        data = self.batch_norm(torch.permute(data, (0, 2, 1)))
+        data = torch.permute(data, (0, 2, 1))
+        layer: torch.nn.Linear
+        for layer in [self.linear_layer_1, self.linear_layer_2, self.linear_layer_3]:
+            data = layer(data)
+            data = torch.relu(data)
+            data = self.dropout(data)
         data = self.final_layer(data)
         return self.softmax(data)
 

aste/models/specialty_models/triplets_extraction.py
@@ -26,18 +26,21 @@ class TripletExtractorModel(BaseModel):
         self.final_metrics: Metric = Metric(name='Final predictions', metrics=metrics).to(config['general']['device'])
 
         input_dimension: int = input_dim * 2
-        self.linear_layer1 = torch.nn.Linear(input_dimension, 300)
-        self.linear_layer2 = torch.nn.Linear(300, 100)
-        # self.linear_layer3 = torch.nn.Linear(input_dimension // 4, input_dimension // 8)
-        # self.linear_layer4 = torch.nn.Linear(input_dimension // 8, 100)
+        self.linear_layer1 = torch.nn.Linear(input_dimension, 500)
+        self.linear_layer2 = torch.nn.Linear(500, 300)
+        self.linear_layer3 = torch.nn.Linear(300, 100)
+        self.linear_layer4 = torch.nn.Linear(100, 100)
         self.final_layer = torch.nn.Linear(100, 6)
         self.dropout = torch.nn.Dropout(0.1)
+        self.batch_norm = torch.nn.BatchNorm2d(input_dimension)
         self.softmax = torch.nn.Softmax(dim=-1)
 
     def forward(self, data: torch.Tensor) -> torch.Tensor:
         matrix_data = self._construct_matrix(data)
+        matrix_data = self.batch_norm(torch.permute(matrix_data, (0, 3, 1, 2)))
+        matrix_data = torch.permute(matrix_data, (0, 2, 3, 1))
         layer: torch.nn.Linear
-        for layer in [self.linear_layer1, self.linear_layer2]:
+        for layer in [self.linear_layer1, self.linear_layer2, self.linear_layer3]:
             matrix_data = layer(matrix_data)
             matrix_data = torch.relu(matrix_data)
             matrix_data = self.dropout(matrix_data)