c21886bb3c0b22b3e2cf90e32703bb8229ebc20c
Iwo Naglik <iwo.naglik98@gmail.com>
2022-06-03 11:05:14 +0200

    SH bugfix

--- experiments/run_experiments.sh
+++ experiments/run_experiments.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 for id in 0 1 2 3 4 5 6
 do
-	python one_experiment.py --dataset_name 14lap --id $id
+        python ./experiments/one_experiment.py --dataset_name 14lap --id $id --save_dir_name original_model
 done
 for id in 0 1 2 3 4 5 6
 do
-	python one_experiment.py --dataset_name 14res --id $id
+        python ./experiments/one_experiment.py --dataset_name 14res --id $id  --save_dir_name original_model
 done
 for id in 0 1 2 3 4 5 6
 do
-        python one_experiment.py --dataset_name 15res --id $id
+        python ./experiments/one_experiment.py --dataset_name 15res --id $id --save_dir_name original_model
 done
 for id in 0 1 2 3 4 5 6
 do
-        python one_experiment.py --dataset_name 16res --id $id
+        python ./experiments/one_experiment.py --dataset_name 16res --id $id  --save_dir_name original_model
 done
