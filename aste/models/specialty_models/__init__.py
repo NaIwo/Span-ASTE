537a43e802a5f89e043c34570edbff8b0dec617f
Iwo Naglik <iwo.naglik98@gmail.com>
2022-07-01 13:41:01 +0200

    Some cleaning

--- aste/models/specialty_models/__init__.py
+++ aste/models/specialty_models/__init__.py
@@ -1,4 +1,3 @@
 from .chunker_model import ChunkerModel
 from ASTE.aste.models.specialty_models.triplets.triplets_extraction import TripletExtractorModel
-from .spans_creator import SpanModel
 from .selection_model import Selector
